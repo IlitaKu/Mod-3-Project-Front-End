cb6d3ebc99d5829ad16ea0dfecf2dd82b9f30366
github username <github email address>
2019-11-13 13:09:29 +0000

    tasks attachedt to categories

--- index.js
+++ index.js
@@ -12,91 +12,73 @@ const categoriesUrl = "http://localhost:3000/categories"
     fetch(categoriesUrl)
         .then(res => res.json())
         .then(data => displayCategories(data))
+        
 
-
-    function displayCategories(categories){
+    function displayCategories(categories) {
         // const name = document.createElement('h4');
         // container is the whole main body.
         const categoriesCollection = document.querySelector('#categories_collection')
         categories.forEach(category => {
             const divFor1Category = document.createElement('div')
-                    const h2tag = document.createElement('h2')
-                        h2tag.innerHTML = category.name
-                    const ulTag = document.createElement('ul')
-                        ulTag.innerHTML= 'List of Tasks:'
-                    const submitButton = document.createElement('button')
-                            submitButton.classList.add('submit')
-                            submitButton.dataset.id = category.id
-                            submitButton.innerText= "New Task"
-            divFor1Category.append(h2tag, ulTag, submitButton)
-            categoriesCollection.appendChild(divFor1Category)
+            const h2tag = document.createElement('h2')
+            h2tag.innerHTML = category.name
+            const ulTag = document.createElement('ul')
+            ulTag.innerHTML = 'List of Tasks:'
+            const submitButton = document.createElement('button')
+            submitButton.classList.add('submit')
+            submitButton.dataset.id = category.id
+            submitButton.innerText = "New Task"
+
             fetchTasksHtml(category.id)
-                .then(taskslist => {
-                    const listedTasks = document.createElement('li')
-                    taskslist.forEach(task => {
-                        listedTasks.innerText = task
+                .then(tasksLi => {
+                    tasksLi.forEach(task => {
+                        const taskListElement = document.createElement('li')
+                        taskListElement.innerHTML = `${task.title}`
+                        const taskDescription = document.createElement('p')
+                        taskDescription.innerHTML = `${task.description}`
+                        taskListElement.append(taskDescription)
+                        ulTag.append(taskListElement)
+                        divFor1Category.append(h2tag, ulTag, submitButton)
+                        categoriesCollection.appendChild(divFor1Category)
                     })
-                    
-                    
-                    debugger
                 })
         })
     }
 
-function createNewTask(newTask){
-    const configObject = {
-        method: 'POST', 
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify(
-          newTask
-        ) 
-    }
-        fetch(taskUrl,configObject)
-}
-
-function displayCards(tasks, categoryId){
+function displayCards(tasks, categoryId) {
     debugger
     const filterredTaskList = tasks.filter(task => task.category_id === categoryId)
-    const list = document.createElement('li')
-    const appendedList = filterredTaskList.map(item => (list.innerHTML = `${item.title}`))
-    // const container = document.querySelector('.container')
-    console.log(appendedList)
-    return appendedList;
+    return filterredTaskList;
 }
 
- const submit = document.querySelector('#js-task-list-form')
- submit.addEventListener('submit', function(e){
-     e.preventDefault()
-     console.log(e.target.value)
-     const title = document.querySelector('.js-task-title-entry').value
-     const description = document.querySelector('.js-task-description-entry').value
-     const newTask = {
-         title,
-         description,
-         category_id : 1
-
-     }
-     createNewTask(newTask)
- })
 
 
-  // fetch tasks for category id
-   // fetchTasksHtml(category.id) // 1
-            //     .then(tasksLi => {
-            //         // debugger
-            //         // append tasks li to the category ul
-            //         const categoryUl = document.createElement('ul');
-            //         const tasklist = document.createElement('li')
-            //         tasklist.innerHTML = tasksLi.join()
-            //         categoryUl.append(tasksLi)
-            //         // categoryUl.append(tasksLi);
-            //         // debugger
-            //         // div.append(name)
-            //         // div.append(categoryUl)
+// function createNewTask(newTask){
+//     const configObject = {
+//         method: 'POST', 
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify(
+//           newTask
+//         ) 
+//     }
+//         fetch(taskUrl,configObject)
+// }
 
-            //         // container.append(div);
-            //         divFor1Category.append(categoryUrl)
-            // })
+//  const submit = document.querySelector('#js-task-list-form')
+//  submit.addEventListener('submit', function(e){
+//      e.preventDefault()
+//      console.log(e.target.value)
+//      const title = document.querySelector('.js-task-title-entry').value
+//      const description = document.querySelector('.js-task-description-entry').value
+//      const newTask = {
+//          title,
+//          description,
+//          category_id : 1
+
+//      }
+//      createNewTask(newTask)
+//  })
+
