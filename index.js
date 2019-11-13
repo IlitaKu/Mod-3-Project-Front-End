c585b9866a217986056fddb755d4edf991d7a499
github username <github email address>
2019-11-13 14:33:28 +0000

    form implementation

--- index.js
+++ index.js
@@ -20,15 +20,20 @@ const categoriesUrl = "http://localhost:3000/categories"
         const categoriesCollection = document.querySelector('#categories_collection')
         categories.forEach(category => {
             const divFor1Category = document.createElement('div')
+            divFor1Category.className = 'card'
+            const divFormHolder = document.createElement('div')
             const h2tag = document.createElement('h2')
             h2tag.innerHTML = category.name
             const ulTag = document.createElement('ul')
-            ulTag.innerHTML = 'List of Tasks:'
+            // ulTag.innerHTML = 'List of Tasks:'
+            // const divForForm = document.createElement('div')
+            // divForForm.classList = 'form-popup'
+            // const formForTask = document.createElement('form')
             const submitButton = document.createElement('button')
             submitButton.classList.add('submit')
             submitButton.dataset.id = category.id
             submitButton.innerText = "New Task"
-
+            // submitButton.addEventListener('click', submitNewTask)
             fetchTasksHtml(category.id)
                 .then(tasksLi => {
                     tasksLi.forEach(task => {
@@ -38,34 +43,41 @@ const categoriesUrl = "http://localhost:3000/categories"
                         taskDescription.innerHTML = `${task.description}`
                         taskListElement.append(taskDescription)
                         ulTag.append(taskListElement)
-                        divFor1Category.append(h2tag, ulTag, submitButton)
-                        categoriesCollection.appendChild(divFor1Category)
+                        divFor1Category.append(h2tag, ulTag )
+                        const formContainer = document.querySelector('.form-container')
+                        divFormHolder.append(formContainer)
+                        divFor1Category.append(divFormHolder)
+                        categoriesCollection.append(divFor1Category)
                     })
                 })
         })
     }
 
 function displayCards(tasks, categoryId) {
-    debugger
+    // debugger
     const filterredTaskList = tasks.filter(task => task.category_id === categoryId)
     return filterredTaskList;
 }
 
+function submitNewTask(e){
+    
+}
 
 
-// function createNewTask(newTask){
-//     const configObject = {
-//         method: 'POST', 
-//         headers: {
-//             "Content-Type": "application/json",
-//             "Accept": "application/json"
-//         },
-//         body: JSON.stringify(
-//           newTask
-//         ) 
-//     }
-//         fetch(taskUrl,configObject)
-// }
+
+    // function createNewTask(newTask){
+    //     const configObject = {
+    //         method: 'POST', 
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             "Accept": "application/json"
+    //         },
+    //         body: JSON.stringify(
+    //           newTask
+    //         ) 
+    //     }
+    //         fetch(taskUrl,configObject)
+    // }
 
 //  const submit = document.querySelector('#js-task-list-form')
 //  submit.addEventListener('submit', function(e){
